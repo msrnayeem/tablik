f01e4e5c9db0ddd1f363e5ca0e85cb751435f45b
Msr Nayeem <msrnayeem@gmail.com>
2023-10-11 23:55:42 +0600

    esha to fajr

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,27 +81,27 @@
                                 <div class="row text-center">
                                     <div class="col-md-2">
                                         <p>Fajr</p>
-                                        <p id="fajr">05:00</p>
+                                        <p id="fajr">00:00</p>
                                     </div>
                                     <div class="col-md-2">
                                         <p>Sunrise</p>
-                                        <p id="sunrise">06:00</p>
+                                        <p id="shurooq">00:00</p>
                                     </div>
                                     <div class="col-md-2">
                                         <p>Dhuhr</p>
-                                        <p id="dhuhr">12:00</p>
+                                        <p id="dhuhr">00:00</p>
                                     </div>
                                     <div class="col-md-2">
                                         <p>Asr</p>
-                                        <p id="asr">15:00</p>
+                                        <p id="asr">00:00</p>
                                     </div>
                                     <div class="col-md-2">
                                         <p>Maghrib</p>
-                                        <p id="maghrib">18:00</p>
+                                        <p id="maghrib">00:00</p>
                                     </div>
                                     <div class="col-md-2">
                                         <p>Isha</p>
-                                        <p id="isha">20:00</p>
+                                        <p id="isha">00:00</p>
                                     </div>
                                 </div>
                             </div>
@@ -117,112 +117,6 @@
 
     <script>
         $(document).ready(function() {
-            const settings = {
-                async: true,
-                crossDomain: true,
-                url: 'https://muslimsalat.p.rapidapi.com/dhaka.json',
-                method: 'GET',
-                headers: {
-                    'X-RapidAPI-Key': '32041a3a6fmsh3304484591e5f79p1bbe88jsn40477c18c2a5',
-                    'X-RapidAPI-Host': 'muslimsalat.p.rapidapi.com'
-                }
-            };
-
-            // Function to fetch prayer times from the API
-            function fetchPrayerTimes() {
-                return $.ajax(settings);
-            }
-
-            // Function to update the HTML elements with prayer times
-            function updatePrayerElements(prayerTimes) {
-                $("#fajr").text(prayerTimes.fajr);
-                $("#sunrise").text(prayerTimes.shurooq);
-                $("#dhuhr").text(prayerTimes.dhuhr);
-                $("#asr").text(prayerTimes.asr);
-                $("#maghrib").text(prayerTimes.maghrib);
-                $("#isha").text(prayerTimes.isha);
-            }
-
-            // Function to display upcoming prayer time live along with the time remaining
-            function displayUpcomingPrayer(prayerTimes) {
-                setInterval(function() {
-                    var currentTime = new Date();
-                    var currentHours = currentTime.getHours();
-                    var currentMinutes = currentTime.getMinutes();
-                    var currentSeconds = currentTime.getSeconds();
-                    var upcomingPrayer = null;
-
-                    // Determine the upcoming prayer
-                    if (currentHours < parseInt(prayerTimes.fajr.split(':')[0]) ||
-                        (currentHours === parseInt(prayerTimes.fajr.split(':')[0]) && currentMinutes <
-                            parseInt(prayerTimes.fajr.split(':')[1]))) {
-                        upcomingPrayer = "Fajr";
-                    } else if (currentHours < parseInt(prayerTimes.dhuhr.split(':')[0]) ||
-                        (currentHours === parseInt(prayerTimes.dhuhr.split(':')[0]) && currentMinutes <
-                            parseInt(prayerTimes.dhuhr.split(':')[1]))) {
-                        upcomingPrayer = "Dhuhr";
-                    } else if (currentHours < parseInt(prayerTimes.asr.split(':')[0]) ||
-                        (currentHours === parseInt(prayerTimes.asr.split(':')[0]) && currentMinutes <
-                            parseInt(prayerTimes.asr.split(':')[1]))) {
-                        upcomingPrayer = "Asr";
-                    } else if (currentHours < parseInt(prayerTimes.maghrib.split(':')[0]) ||
-                        (currentHours === parseInt(prayerTimes.maghrib.split(':')[0]) &&
-                            currentMinutes < parseInt(prayerTimes.maghrib.split(':')[1]))) {
-                        upcomingPrayer = "Maghrib";
-                    } else if (currentHours < parseInt(prayerTimes.isha.split(':')[0]) ||
-                        (currentHours === parseInt(prayerTimes.isha.split(':')[0]) && currentMinutes <
-                            parseInt(prayerTimes.isha.split(':')[1]))) {
-                        upcomingPrayer = "Isha";
-                    } else {
-                        upcomingPrayer = "Fajr";
-                    }
-
-                    // Calculate time remaining to the upcoming prayer
-                    var upcomingPrayerTime = new Date();
-                    upcomingPrayerTime.setHours(parseInt(prayerTimes[upcomingPrayer.toLowerCase()].split(
-                        ':')[0]));
-                    upcomingPrayerTime.setMinutes(parseInt(prayerTimes[upcomingPrayer.toLowerCase()].split(
-                        ':')[1]));
-                    upcomingPrayerTime.setSeconds(0); // Reset seconds to zero for accuracy
-
-                    var timeDiff = upcomingPrayerTime - currentTime;
-                    var hoursRemaining = Math.floor(timeDiff / (1000 * 60 * 60));
-                    var minutesRemaining = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-                    var secondsRemaining = Math.floor((timeDiff % (1000 * 60)) / 1000);
-
-                    // Format remaining time as hh:mm:ss
-                    var formattedTime = padZero(hoursRemaining) + ":" + padZero(minutesRemaining) + ":" +
-                        padZero(secondsRemaining);
-
-                    // Display upcoming prayer time along with time remaining
-                    $("#time").text(upcomingPrayer + " - " + formattedTime);
-                }, 1000);
-            }
-
-            // Helper function to pad zero to single-digit numbers
-            function padZero(number) {
-                return (number < 10 ? '0' : '') + number;
-            }
-
-
-
-            // Main function to fetch, update, and display prayer times
-            function updatePrayerTimes() {
-                fetchPrayerTimes()
-                    .done(function(response) {
-                        console.log(response.items);
-                        var prayerTimes = response.items[0];
-                        updatePrayerElements(prayerTimes);
-                        displayUpcomingPrayer(prayerTimes);
-                    })
-                    .fail(function(xhr, status, error) {
-                        console.error("Request failed: " + status + " - " + error);
-                    });
-            }
-
-            // Call the main function to fetch, update, and display prayer times
-            updatePrayerTimes();
-
 
             $.ajax({
                 url: "https://api.aladhan.com/v1/gToH",
@@ -242,37 +136,162 @@
                 }
             });
 
+
+            var keyValueArray = [];
+            const settings = {
+                async: true,
+                crossDomain: true,
+                url: 'https://muslimsalat.p.rapidapi.com/dhaka.json',
+                method: 'GET',
+                headers: {
+                    'X-RapidAPI-Key': '32041a3a6fmsh3304484591e5f79p1bbe88jsn40477c18c2a5',
+                    'X-RapidAPI-Host': 'muslimsalat.p.rapidapi.com'
+                }
+            };
+
+            var prayerTimesArray = [];
+
+            $.ajax(settings).done(function(response) {
+                var prayerTimes = response.items[0];
+                var isFirstItem = true;
+
+                for (var key in prayerTimes) {
+                    if (prayerTimes.hasOwnProperty(key)) {
+                        if (isFirstItem) {
+                            isFirstItem = false;
+                            continue; // Skip the first key-value pair
+                        }
+
+                        prayerTimesArray.push({
+                            key: key,
+                            value: prayerTimes[key]
+                        });
+                    }
+                }
+
+                updatePrayerElements(prayerTimesArray);
+
+                var upcomingPrayerInfo = findUpcomingPrayer(prayerTimesArray);
+
+            });
+
+            function updatePrayerElements(prayerTimesArray) {
+                for (var i = 0; i < prayerTimesArray.length; i++) {
+                    var key = prayerTimesArray[i].key;
+                    var value = prayerTimesArray[i].value;
+                    $("#" + key).text(value);
+                }
+            }
+
+
+            function findUpcomingPrayer(prayerTimesArray) {
+                var currentTime = new Date();
+                var upcomingPrayerTime;
+                var upcomingPrayerName;
+
+                for (var i = 0; i < prayerTimesArray.length; i++) {
+                    var prayerTime = new Date(prayerTimesArray[i].value);
+
+                    // Check if the prayer time is in the future
+                    if (prayerTime > currentTime) {
+                        upcomingPrayerTime = prayerTime;
+                        upcomingPrayerName = prayerTimesArray[i].key;
+                        break;
+                    }
+                }
+
+                if (upcomingPrayerTime) {
+                    var timeDifference = upcomingPrayerTime - currentTime;
+                    var hoursRemaining = Math.floor(timeDifference / (1000 * 60 * 60));
+                    var minutesRemaining = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+                    var secondsRemaining = Math.floor((timeDifference % (1000 * 60)) / 1000);
+                    time
+                    console.log("Upcoming Prayer: " + upcomingPrayerName);
+                    console.log("Time remaining: " + hoursRemaining + "h " + minutesRemaining + "m " +
+                        secondsRemaining + "s");
+                } else {
+                    //calculate esha to fajr difference
+                    var currentTime = new Date();
+                    var currentHours = currentTime.getHours();
+                    var currentMinutes = currentTime.getMinutes();
+
+                    var currentTimeInMin = currentHours * 60 + currentMinutes;
+                    var nextDayFajr = convertToMinutes(prayerTimesArray[0].value);
+
+
+                    var remainingTime = nextDayFajr - currentTimeInMin + (24 * 60);
+                    // Convert remaining time back to hours and minutes
+                    var remainingHours = Math.floor(remainingTime / 60);
+                    var remainingMinutes = remainingTime % 60;
+
+                    // Add leading zeros if necessary
+                    if (remainingHours < 10) {
+                        remainingHours = "0" + remainingHours;
+                    }
+
+                    if (remainingMinutes < 10) {
+                        remainingMinutes = "0" + remainingMinutes;
+                    }
+
+                    // Create the formatted time string
+                    var time = remainingHours + " : " + remainingMinutes;
+
+                    updateClock("Fajr : ", time);
+                }
+            }
+
+
+            function convertToMinutes(timeString) {
+                var timeArray = timeString.split(":");
+                var hours = parseInt(timeArray[0], 10);
+                var minutes = parseInt(timeArray[1], 10);
+                return hours * 60 + minutes;
+            }
+
+            function updateClock(prayername, timeString) {
+                // Split the time string into hours and minutes
+                var timeArray = timeString.split(" : ");
+                var hours = parseInt(timeArray[0], 10);
+                var minutes = parseInt(timeArray[1], 10);
+
+                // Update the clock every second
+                var interval = setInterval(function() {
+                    // Decrease one second
+                    seconds--;
+                    if (seconds < 0) {
+                        seconds = 59;
+                        if (minutes === 0) {
+                            minutes = 59;
+                            if (hours === 0) {
+                                // Stop the clock when time is up
+                                clearInterval(interval);
+                                console.log("Time's up!");
+                            } else {
+                                hours--;
+                            }
+                        } else {
+                            minutes--;
+                        }
+                    }
+
+                    // Format hours, minutes, and seconds with leading zeros if necessary
+                    var Hours = hours < 10 ? "0" + hours : hours;
+                    var Minutes = minutes < 10 ? "0" + minutes : minutes;
+                    var Seconds = seconds < 10 ? "0" + seconds : seconds;
+
+                    // Update the clock display
+                    $("#time").text(prayername + Hours + " : " + Minutes + " : " + Seconds);
+
+                }, 1000); // Update every second (1000 milliseconds)
+
+                // Initial seconds
+                var seconds = 0;
+            }
+
+
+
+
         });
-
-        // function updateClock() {
-        //     var now = new Date();
-        //     var hours = now.getHours();
-        //     var minutes = now.getMinutes();
-        //     var seconds = now.getSeconds();
-        //     var ampm = hours >= 12 ? 'PM' : 'AM';
-
-        //     // Convert hours to 12-hour format
-        //     hours = hours % 12;
-        //     hours = hours ? hours : 12; // The hour '0' should be '12' in 12-hour clock
-
-        //     // Format the time to add leading zeros
-        //     hours = hours < 10 ? '0' + hours : hours;
-        //     minutes = minutes < 10 ? '0' + minutes : minutes;
-        //     seconds = seconds < 10 ? '0' + seconds : seconds;
-
-        //     var timeString = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
-
-
-        //  Update the clock and date elements
-        //       document.getElementById('clock').textContent = timeString;
-
-        // }
-
-        // // Update clock every second
-        //    setInterval(updateClock, 1000);
-
-        // Call updateClock function immediately to display clock without delay
-        // updateClock();
     </script>
 </body>
 
